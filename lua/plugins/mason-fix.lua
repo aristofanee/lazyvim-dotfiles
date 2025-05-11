399b7eb6a27b17b06faaeedeebdfe721f5f3ccfa
Davide Notaro <dav.notaro@gmail.com>
2025-05-11 13:20:38 +0200

    added stuff for fixing a bug in the Mason lsp config

---
+++ lua/plugins/mason-fix.lua
@@ -0,0 +1,4 @@
+return {
+--  { "mason-org/mason.nvim", version = "1.11.0" }, -- Example version, check for recommended versions
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" }, -- Example version
+}
