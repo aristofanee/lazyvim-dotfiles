b3ec9453d13fa433f7ae51ca239caf83007ea4b8
Davide Notaro <dav.notaro@gmail.com>
2025-05-22 19:25:37 +0200

    added the check for the os

--- init.lua
+++ init.lua
@@ -5,13 +5,11 @@ vim.g.neovide_font = "JetBrainsMonoNL"
 vim.g.neovide_cursor_animation_length = 0
 
 -- Set PowerShell as the default shell
+if vim.fn.has('win32') == 1 then
 vim.opt.shell = "powershell.exe -NoLogo"
-
 vim.opt.shellxquote = '"'
-
 -- Or, for the built-in Windows PowerShell:
 -- vim.opt.shell = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'
-
 -- Optional: Set the shell to execute commands
 -- This tells Neovim how to pass commands to the shell.
 -- It's good practice to set this with PowerShell for proper command execution.
@@ -20,6 +18,8 @@ vim.opt.shellcmdflag = '-NoLogo -NoProfile -Command'
 -- Optional: Set the program used to read the shell output
 -- This ensures Neovim can correctly interpret the output stream.
 vim.opt.shellxquote = '' -- No quotes needed for PowerShell
+end
+
 vim.opt.shellredir = '> %s 2>&1' -- Redivim.opt.updatetime = 100       -- Quick UI updates
 
 -- Stuff done for optimization
