95d1efa07759d8de46cc73203821945de1307139
Davide Notaro <Davide Notaro>
2025-05-24 18:51:30 +0200

    updated the dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -28,13 +28,13 @@ return {
           -- stylua: ignore
           ---@type snacks.dashboard.Item[]
           keys = {
-            { icon = " ", key = "f", desc = "find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "new File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = " ", key = "r", desc = "recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "f", desc = "find file", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "new file", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "find text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "recent files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             { icon = " ", key = "c", desc = "config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-            { icon = " ", key = "s", desc = "restore Session", section = "session" },
-            { icon = " ", key = "x", desc = "lazy Extras", action = ":LazyExtras" },
+            { icon = " ", key = "s", desc = "restore session", section = "session" },
+            { icon = " ", key = "x", desc = "lazy extras", action = ":LazyExtras" },
             { icon = "󰒲 ", key = "l", desc = "lazy", action = ":Lazy" },
             { icon = " ", key = "q", desc = "quit", action = ":qa" },
           },
