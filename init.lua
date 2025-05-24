8baf34a996e7e5c0fd6216dfb277db8ff4a0ebff
Davide Notaro <Davide Notaro>
2025-05-24 19:57:42 +0200

    added a little bit more transparency, still not perfect

--- init.lua
+++ init.lua
@@ -20,6 +20,8 @@ if vim.fn.has("win32") == 1 then
   vim.opt.shellxquote = "" -- No quotes needed for PowerShell
 end
 
+vim.opt.termguicolors = true
+
 vim.opt.shellredir = "> %s 2>&1" -- Redivim.opt.updatetime = 100       -- Quick UI updates
 
 -- Stuff done for optimization
@@ -28,3 +30,41 @@ vim.opt.timeoutlen = 300 -- Faster key sequence completion
 vim.opt.lazyredraw = false -- Draw screen during macros (your CPU can handle it)
 vim.opt.ttyfast = true -- Faster terminal connection (though usually on by default in Neovim)
 vim.g.cursorline_timeout = 100 -- If you use cursorline pluginrect stderr to stdout
+
+vim.cmd([[
+  " Basic transparency
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NonText guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+  
+  " Line numbers and gutter
+  highlight LineNr guibg=NONE ctermbg=NONE
+  highlight CursorLineNr guibg=NONE ctermbg=NONE
+  highlight LineNrAbove guibg=NONE ctermbg=NONE
+  highlight LineNrBelow guibg=NONE ctermbg=NONE
+  
+  " Terminal
+  highlight Terminal guibg=NONE ctermbg=NONE
+  highlight TerminalNormal guibg=NONE ctermbg=NONE
+  
+  " File explorer/tree
+  highlight NeoTreeNormal guibg=NONE ctermbg=NONE
+  highlight NeoTreeNormalNC guibg=NONE ctermbg=NONE
+  highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+  highlight NvimTreeNormal guibg=NONE ctermbg=NONE
+  highlight NvimTreeEndOfBuffer guibg=NONE ctermbg=NONE
+  highlight NvimTreeVertSplit guibg=NONE ctermbg=NONE
+  
+  " Window separators
+  highlight VertSplit guibg=NONE ctermbg=NONE
+  highlight WinSeparator guibg=NONE ctermbg=NONE
+  
+  " Status line
+  highlight StatusLine guibg=NONE ctermbg=NONE
+  highlight StatusLineNC guibg=NONE ctermbg=NONE
+  
+  " Folding
+  highlight Folded guibg=NONE ctermbg=NONE
+  highlight FoldColumn guibg=NONE ctermbg=NONE
+]])
